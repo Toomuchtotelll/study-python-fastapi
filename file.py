d76eff5a0143fc4cd5206dca47be9a65202f555c
halyna.stepchuk <halyna.stepchuk@ip-192-168-31-241.eu-central-1.compute.internal>
2026-01-05 20:47:02 +0200

    3 урок

--- file.py
+++ file.py
@@ -1,1 +1,12 @@
-print("hello world")
+a = int(input())
+b = int(input())
+
+for i in range(a,b+1):
+    if i > 1:
+        is_prime = True
+        for j in range(2,i):
+            if i % 2 == 0 or i % 3 == 0:
+                is_prime = False
+                break
+        if is_prime == True:
+            print(i)
